ef8a23f9e0d5bb99c47776e50d700f4cce6bfeaa
Ritika Chandwani <ritikachandwani09@gmail.com>
2022-06-23 00:38:34 +0530

    Message boxes created and enabled

--- server/index.js
+++ server/index.js
@@ -24,6 +24,12 @@ io.on("connection", (socket) => {
         socket.emit('welcome', { user: "Admin", message: `Welcome to the chat,${users[socket.id]}` })
     })
 
+    socket.on('message', ({ message, id }) => {
+        // we want the message to show to everyone so using io
+        io.emit('sendMessage', { user: users[id], message, id });
+
+    })
+
     socket.on('disconnected', () => {
         socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]} has left`})
         console.log("user left")
